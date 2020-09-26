99bf3cdf6337cd7d57ea57c11c70e7c694d34064
Leannaland <leannaland2011@gmail.com>
2020-09-26 13:21:42 -0400

    commit JS and HTML files to Project 1

---
+++ Script.js
@@ -0,0 +1,24 @@
+//Variables//
+
+
+// When the search button is clicked
+      //$("id").click(function(){
+             //page retuns a new page.
+            //retuns songs info in Playlist
+                //API Call
+                    //....
+             // itunes API
+                //API Call
+                    //...
+      //})
+   
+//
+
+
+//When song from the paylist is clicked lyrics sections retuns lyrics 
+ 
+        //returns songs lyrics
+          //$("id").click(function(){
+               //Click event on playlist list items returns lyrics 
+               //APi Call
+          //})
