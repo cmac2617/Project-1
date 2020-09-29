5ac89f66c0e090958fb9b57f432622cb54b89401
Leannaland <leannaland2011@gmail.com>
2020-09-28 21:11:57 -0400

    html update

--- Script.js
+++ Script.js
@@ -1,3 +1,35 @@
+
+
+var bpm = "";
+$("#bpmSearch").click(function() {
+    bpm = $("#bpminput").val();
+    console.log($("#bpmSearch").val());
+    console.log(bpm);
+});
+$("#bpmSearch").click(function() {$.ajax({
+    url: `https://api.getsongbpm.com/tempo/?api_key=57452074649ab146238866e6411b82b5&bpm=${bpm}`,
+    method: "GET"
+})
+    // After the data comes back from the API
+    .then(function (response) {
+        // Storing an array of results in the results variable
+        var results = response;
+        console.log(results);
+    })
+});
+//var results1 = "";
+    //$.ajax({
+       // url: "https://itunes.apple.com/search?term=jack+johnson",
+        //method: "GET"
+    //})
+    //.then(function(response) {results1 = response;
+        //console.log(results1);
+    //})
+
+
+
+
+
 //Variables//
 
 
