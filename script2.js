e35e345afa539515b0689b7ab6ffff82e79a7aa3
Cyle <cmac2617@yahoo.com>
2020-10-07 17:39:36 -0400

    Final changes.

--- script2.js
+++ script2.js
@@ -1,130 +1,85 @@
-//put all of this in $(document).on("ready.......")
+// Declare variables.
 var bpm = localStorage.getItem("bpmStringified");
 var artistList = [];
 var playList = [];
-var bpmList = [];
-console.log(bpm);
+
+// Give an alert if bpm search is not within acceptable range, and direct to try again.
+if (bpm < 40 || bpm > 220) {
+    alert("BPM value must be within range 40-220. Click the option to generate a new playlist and try again.")
+}
+
+// Run AJAX call to get 10 songs and artists whose songs match the search for BPM.
+// Build arrays to store the artists and songs. Use artistList array to run for loop
+// through all artists and display on page. Both arrays will also be used in the
+// third AJAX call to specify paramaters for the queryURL.
 $.ajax({
-  url: "https://api.getsongbpm.com/tempo/?api_key=328b19bb2055fb8c628bfe2d7d83508d&bpm=" + bpm,
-  method: "GET",
-  async: false
+    url: "https://api.getsongbpm.com/tempo/?api_key=328b19bb2055fb8c628bfe2d7d83508d&bpm=" + bpm,
+    method: "GET",
+    async: false
 })
-  // After the data comes back from the API
-  .then(function (response) {
-      // Storing an array of results in the results variable
-      response = JSON.parse(response);
-      var i = 0;
-      console.log(response);
-      for (i = 0; i < 10; i++) {
-          var songTitle = $("<li>").text(response.tempo[i].song_title + " by " + response.tempo[i].artist.name);
-          songTitle.attr({class: "lyricLink", id: i});
-        $("#box1").append(songTitle);
-    //    .then(function (response) {
-    //         response = JSON.parse(response).results;
-    //         var match;
-    //         for (var i = 0; i < response.length; i++) {
-    //             if (response[i].artistName === song.artist) {
-    //                 match = response[i];
-    //                 break;
-    //             }
-    //         }
-    //         console.log(match)
-    //     })
-    //     .catch(function (err) {
-    //         console.log(err)
-    //     })
-          artistList.push(response.tempo[i].artist.name);
-          playList.push(response.tempo[i].song_title);
-          bpmList.push(response.tempo[i].tempo);
-      }
-      console.log(artistList);
-      console.log(playList);
-      console.log(bpmList);
-  var links = setInterval(function () {
-    if (artistList.length >= 10) {
-        
-        for (b = 0; b < 10; b++) {
-            $.ajax({
-                url: "https://itunes.apple.com/search?term=" + artistList[b] + "&media=music",
-                method: "GET",
-                async: false
-            })
-                .then(function (responser) {
-                    console.log(responser);
-                    console.log(responser.results[b].artistViewUrl)
-                    // var artistLink = $("<li>").text(response.results[i].artistViewUrl);
-                    var newArtist = $("<li>");
-                    // newArtist.attr("class", "newLink");
-                    var artistLink = $("<a>").text(responser.results[b].artistName);
-                    artistLink.attr("href", responser.results[b].artistViewUrl);
-        $("#box2").append(newArtist);
-        $(newArtist).append(artistLink);
-                })
+    .then(function (response) {
+        response = JSON.parse(response);
+        var i = 0;
+        for (i = 0; i < 10; i++) {
+            var songTitle = $("<li>").text(response.tempo[i].song_title + " by " + response.tempo[i].artist.name);
+
+            // Specify a "lyricLink" class and indexed id, to be used later when click each song to generate lyrics.
+            songTitle.attr({ class: "lyricLink", id: i });
+
+            // Push the song title to the page.
+            $("#box1").append(songTitle);
+
+            // Build artistList array and playList array.
+            artistList.push(response.tempo[i].artist.name);
+            playList.push(response.tempo[i].song_title);
         }
-        clearInterval(links);
-    }
-}, 2000);
-// var lyrics = setInterval(function() {
-//     if (playList.length >= 10) {
-//         console.log("working");
-//         var i = 0;
-//         for (i == 0; i < playList.length; i++) {
-//             $.ajax({
-//                 url: "https://orion.apiseeds.com/api/music/lyric/" + artistList[i] + "/" + playList[i] + "?apikey=jHfKMGQkqpG8PUmPiWEAMXUg5h10bEhKI6cduUVaS5qr0Ap5LwRCFe1zAMpQK1Eg ",
-//                 method: "GET",
-//                 async: false
-//             })
-//                 .then(function (response) {
-//                     var newLyrics = $("<p>").text(response.result.track.text);
-//                     $("#box3").append(newLyrics);
-//                 })
-//         }
-//         clearInterval(lyrics);
-//         }
-// }, 2000);
 
+        // Use set interval function to keep checking for artistList array to equal 10.
+        // Once it does, generate the artist links to their music pages using the built array and exit.
+        var links = setInterval(function () {
+            if (artistList.length >= 10) {
 
+                // for loop to run through artistList and place artist's pages on screen.
+                for (b = 0; b < 10; b++) {
+                    $.ajax({
+                        url: "https://itunes.apple.com/search?term=" + artistList[b] + "&media=music",
+                        method: "GET",
+                        async: false
+                    })
+                        .then(function (responser) {
+                            var newArtist = $("<li>");
+                            var artistLink = $("<a>").text(responser.results[b].artistName);
+                            artistLink.attr("href", responser.results[b].artistViewUrl);
+                            $("#box2").append(newArtist);
+                            $(newArtist).append(artistLink);
+                        })
+                }
+                clearInterval(links);
+            }
+        }, 1000);
+    })
 
+// Click funtion to be used for every song in the playlist.
+// Generates lyrics to songs at the bottom of the screen.
+$(document).on('click', '.lyricLink', function () {
 
-
-
-
-
-})
-
-
-// $(document).on('click', '.lyricLink', function(){
-//     console.log("You clicked a song.");
-// });
-// $(document).on('mouseover', '.lyricLink', function(){
-//     $('.lyricLink').css('cursor', 'pointer');
-// });
-
-
-
-
-
-
-
-$(document).on('click', '.lyricLink', function(){
-    console.log("You clicked a song.");
+    // Specify variable to use unique id that was applied to the song in the first API call.
     var x = $(this).attr("id");
-    console.log(x);
-    
-        $.ajax({
-            url: "https://orion.apiseeds.com/api/music/lyric/" + artistList[x] + "/" + playList[x] + "?apikey=jHfKMGQkqpG8PUmPiWEAMXUg5h10bEhKI6cduUVaS5qr0Ap5LwRCFe1zAMpQK1Eg ",
-            method: "GET"
-        })
-            .then(function (response) {
-                
-                // var newLyrics = $("<p>").text(response.result.track.text);
-                $("#box3").text(response.result.track.text);
-            })
 
-    
-    
+    // Specify correct artist and song from artistList and playList arrays,
+    // based on the unique id.
+    $.ajax({
+        url: "https://orion.apiseeds.com/api/music/lyric/" + artistList[x] + "/" + playList[x] + "?apikey=jHfKMGQkqpG8PUmPiWEAMXUg5h10bEhKI6cduUVaS5qr0Ap5LwRCFe1zAMpQK1Eg ",
+        method: "GET"
+    })
+        .then(function (response) {
+
+            // Append lyrics to div with id "box3", at the bottom of the page.
+            $("#box3").text(response.result.track.text);
+        })
 });
 
-$(document).ajaxError(function(){
+// If lyrics can't be found for the song, display message.
+$(document).ajaxError(function () {
     $("#box3").text("Could not obtain lyrics.");
 })
